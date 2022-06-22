2bc1c6be7ee4c13306cd158d7cc6f238516c9aee
ioxu <ben@ioxu.com>
2022-06-22 07:14:50 +0100

    3d under ViewportContainer

--- globals/game.gd
+++ globals/game.gd
@@ -1,9 +1,9 @@
-extends Spatial
+extends Node2D
 
 
 func _ready():
 	print("[game] starting %s"%self.get_path())
-	print("[game] level %s"% $default_level.get_path())
+	print("[game] level %s"% $ViewportContainer/default_level.get_path())
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
@@ -11,4 +11,5 @@ func _input(_event):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
-
+func _process(delta):
+	$fps_label.text = str(Engine.get_frames_per_second())
