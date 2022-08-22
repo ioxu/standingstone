12f143166145f9dd4bfdd7b7e2e09721b261c040
ioxu <ben@ioxu.com>
2022-08-22 09:15:32 +0100

    adding some graphics settings ui controls

--- globals/menu.gd
+++ globals/menu.gd
@@ -1,8 +1,19 @@
 extends MarginContainer
 
+export(NodePath) onready var graphics_settings_menu = get_node(graphics_settings_menu)
+
 
 func _ready() -> void:
+	self.graphics_settings_menu.set_visible( false )
 	self.deactivate()
+
+
+	var msaa_options = self.find_node("msaa_options")
+	msaa_options.add_item("Disabled")
+	msaa_options.add_item("2 x")
+	msaa_options.add_item("4 x")
+	msaa_options.add_item("8 x")
+	msaa_options.add_item("16 x")
 
 
 func _input(event: InputEvent) -> void:
@@ -28,7 +39,11 @@ func activate()->void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
+func _on_graphics_settings_button_toggled(button_pressed: bool) -> void:
+	pprint("show graphics settings menu: %s"%button_pressed)
+	graphics_settings_menu.set_visible( button_pressed )
+
+
 func pprint(thing) -> void:
 	print("[menu] %s"%str(thing))
-
 
