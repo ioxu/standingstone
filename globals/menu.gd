cbbb447e6fa139cf424b7cff04430f8e688368d4
ioxu <ben@ioxu.com>
2022-08-29 18:13:49 +0100

    connect motionblur menu controls

--- globals/menu.gd
+++ globals/menu.gd
@@ -5,6 +5,7 @@ export(NodePath) onready var graphics_settings_menu = get_node(graphics_settings
 export(NodePath) onready var render_viewport = get_node(render_viewport)
 export(NodePath) onready var display_texture = get_node(display_texture)
 export(NodePath) onready var ui_debug_root = get_node(ui_debug_root)
+export(NodePath) onready var camera = get_node(camera)
 
 var MSAA_ENUM_STRINGS = ["Viewport.MSAA_DISABLED",
 	"Viewport.MSAA_2X",
@@ -136,3 +137,10 @@ func _on_debug_render_info_checkbox_toggled(button_pressed: bool) -> void:
 
 func _on_resolutions_info_checkbox_toggled(button_pressed: bool) -> void:
 	ui_debug_root.find_node( "resolutions_info_container" ).set_visible( button_pressed )
+
+#-------------------------------------------------------------------------------
+# effects
+func _on_motionblur_enable_checkbox_toggled(button_pressed: bool) -> void:
+	camera.find_node("motion_blur").set_visible( button_pressed )
+	self.find_node("motionblur_iterations").set_editable( button_pressed )
+	self.find_node("motionblur_intensity").set_editable( button_pressed )
